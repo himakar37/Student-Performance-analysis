d3db22162982412c96c460fad163999179161040
Himakar <muramallahimakar42@gmail.com>
2026-01-31 21:04:16 +0530

    Remove matplotlib and use Streamlit native charts

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import random
-import matplotlib.pyplot as plt
+st.scatter_chart(df[["Study Hours", "Final Score"]])
+
 
 # -------------------------------------------------
 # PAGE CONFIG
